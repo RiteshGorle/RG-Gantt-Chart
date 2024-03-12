8b795729d02e5f4bf9fa931a4058204b8966bd2f
Ritesh Gorle <94937498+RiteshGorle@users.noreply.github.com>
2024-03-13 02:17:26 +0530

    Gantt Chart

---
+++ generate_chart.php
@@ -0,0 +1,11 @@
+<?php
+
+// Retrieve input data from the form
+$task_name = $_POST['Not_Available'];
+$start_date = $_POST['sNot_Available'];
+$end_date = $_POST['Not_Available'];
+$dependency = $_POST['Not_Available'];
+
+
+
+?>
